eaab77a0ba7a2639de90fe886a96844ff9b3814c
cqh963852 <cqh963852@hotmail.com>
2023-07-27 23:28:17 +0800

    fix: remove broken link

--- apps/website/docusaurus.config.js
+++ apps/website/docusaurus.config.js
@@ -45,13 +45,6 @@ const config = {
           editUrl:
             "https://github.com/idealjs/jellyfish/tree/main/packages/create-docusaurus/templates/shared/",
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/idealjs/jellyfish/tree/main/packages/create-docusaurus/templates/shared/",
-        },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
         },
@@ -77,7 +70,6 @@ const config = {
             position: "left",
             label: "Tutorial",
           },
-          { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/idealjs/jellyfish",
             label: "GitHub",
@@ -100,10 +92,6 @@ const config = {
           {
             title: "More",
             items: [
-              {
-                label: "Blog",
-                to: "/blog",
-              },
               {
                 label: "GitHub",
                 href: "https://github.com/idealjs/jellyfish",
