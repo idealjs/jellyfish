9235d4b1e9d9b10fabe96beca3346fae32a0ec45
cqh963852 <cqh963852@hotmail.com>
2023-07-30 20:23:57 +0800

    fix: add Suspense

--- apps/website/docs/components/PTGDemo.tsx
+++ apps/website/docs/components/PTGDemo.tsx
@@ -1,13 +1,15 @@
 import BrowserOnly from "@docusaurus/BrowserOnly";
 import { PTG, sphereShape } from "@idealjs/ptg";
-import React from "react";
+import React, { Suspense } from "react";
 
 const PTGDemo = () => {
   return (
     <BrowserOnly fallback={<div />}>
       {() => {
         return (
-          <PTG width={500} height={500} noiseSeed={0} shape={sphereShape} />
+          <Suspense fallback={null}>
+            <PTG width={500} height={500} noiseSeed={0} shape={sphereShape} />
+          </Suspense>
         );
       }}
     </BrowserOnly>
