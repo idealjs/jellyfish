e6404d574e47dcdee01a5abbd72c719ec84e8d35
uito-shen <uito.shen@outlook.com>
2023-07-30 20:46:30 +1000

    feat: find border and neighbors

--- packages/ptg/src/utils/voronio.ts
+++ packages/ptg/src/utils/voronio.ts
@@ -2,9 +2,10 @@ import { Delaunay } from "d3-delaunay";
 import type P5 from "p5";
 import { Vector } from "p5";
 
-interface VoronoiCell {
+export interface VoronoiCell {
   site: Vector;
   polygon: Vector[];
+  neighbors?: number[];
 }
 
 export class VoronoiDiagram {
@@ -17,7 +18,7 @@ export class VoronoiDiagram {
     width: number,
     height: number
   ) {
-    for (let i = 0; i < iterations; i++) {
+    for (let iter = 0; iter < iterations; iter++) {
       const delaunay = Delaunay.from(points.map((p) => [p.x, p.y]));
       const voronoi = delaunay.voronoi([0, 0, width, height]);
 
@@ -28,11 +29,12 @@ export class VoronoiDiagram {
           polygon: polygon
             ? polygon.map(([x, y]) => p5.createVector(x, y))
             : [],
+          neighbors: Array.from(delaunay.neighbors(index)),
         };
       });
 
       // For all further iterations, move the points to the centroid of their cell
-      if (i < iterations - 1) {
+      if (iter < iterations - 1) {
         points = this.cells.map((cell) => this.centroid(p5, cell.polygon));
       }
     }
